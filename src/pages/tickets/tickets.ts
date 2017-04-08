d2bebf3719d715e208e77d37d613f837b2fcde04
Kleo <kleo@Helius-Software.local>
2017-04-08 18:59:54 +0200

    WIP: tickets

--- src/pages/tickets/tickets.ts
+++ src/pages/tickets/tickets.ts
@@ -14,14 +14,86 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class Tickets {
     dates: any[];
+    tickets: any[];
 
     constructor(public navCtrl: NavController, public navParams: NavParams) {
-        this.dates = ['12 March', '14 April', '16 April'];
+
+        this.dates = [];
+
+        this.tickets = [
+            {
+                id: 0,
+                date: '2017-02-14',
+                time: '12:00',
+                location: 'Rr e Barrikadave, Tirane'
+            },
+            {
+                id: 1,
+                date: '2017-02-14',
+                time: '14:30',
+                location: 'Rr e Barrikadave, Tirane'
+            },
+            {
+                id: 0,
+                date: '2017-03-08',
+                time: '14:00',
+                location: 'Rr e Barrikadave, Tirane'
+            },
+            {
+                id: 0,
+                date: '2017-04-12',
+                time: '11:00',
+                location: 'Rr e Barrikadave, Tirane'
+            }
+        ];
+
+        this.tickets.sort((a, b) => {
+            let dateA = new Date(a.date);
+            let dateB = new Date(b.date);
+            if (dateA < dateB)
+                return -1;
+            else
+                return 1;
+        });
+
+        console.log(this.tickets);
+
+        this.tickets.forEach((ticket) => {
+            let obj = {day: null, month: null}
+            let date = new Date(ticket.date);
+            obj.day = date.getDate();
+            obj.month = this.getMonthName(date.getMonth());
+            console.log(this.existsInArray(obj, this.dates));
+            if (!this.existsInArray(obj, this.dates)){
+                this.dates.push(obj);
+            }
+        });
+        
+        // this.dates = this.tickets.map((ticket) => {
+        //     let obj = {day: null, month: null}
+        //     let date = new Date(ticket.date);
+        //     obj.day = date.getDate();
+        //     obj.month = this.getMonthName(date.getMonth());
+        //     return obj;
+        // });
     }
 
     ionViewDidLoad() {
         console.log('ionViewDidLoad Tickets');
         console.log(this.dates);
+    }
+
+    getMonthName(index){
+        const months = ['Janar', 'Shkurt', 'Mars', 'Prill', 'Maj', 'Qershor', 'Korrik', 'Gusht', 'Shtator', 'Tetor', 'Nentor', 'Dhjetor'];
+        return months[index];
+    }
+
+    existsInArray(obj, arr){
+        return arr.forEach((el) => {
+            if (JSON.stringify(obj) == JSON.stringify(el))
+                return true;
+        });
+        // return false;
     }
 
 }
